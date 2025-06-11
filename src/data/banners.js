504dd23657010d08f85a8e36297a8fe4c086ed76
ProgAbhi <abhishekpandey678@gmail.com>
2025-06-11 17:38:11 +0530

    updated banners

--- src/data/banners.js
+++ src/data/banners.js
@@ -1,5 +1,5 @@
 export const banners = [
-  { id: 1, image: "/images/banner1.jpg", title: "Welcome to Our Site", url: "https://traveltriangle.com/" },
-  { id: 2, image: "/images/banner2.jpg", title: "Discover Features", url: "https://traveltriangle.com/blog/"},
-  { id: 3, image: "/images/banner3.jpg", title: "Get Started Now", url: "https://traveltriangle.com/tour-packages"},
+  { id: 1, image: "https://unsplash.com/photos/a-mountain-covered-in-snow-with-trees-in-the-foreground-Wp50rAhk_vQ", title: "Welcome to Our Site", url: "https://traveltriangle.com/" },
+  { id: 2, image: "https://unsplash.com/photos/sunset-shines-through-pier-columns-on-the-beach-X4Dv9ftAhvQ", title: "Discover Features", url: "https://traveltriangle.com/blog/"},
+  { id: 3, image: "https://unsplash.com/photos/city-skyline-with-bridge-lights-up-at-night-Xxc8eHJ04J8", title: "Get Started Now", url: "https://traveltriangle.com/tour-packages"},
 ];
